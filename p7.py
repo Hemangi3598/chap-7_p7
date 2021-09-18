1d6926518c6f6ab928fc45adbf2eb2d433664434
Hemangi3598 <90692254+Hemangi3598@users.noreply.github.com>
2021-09-18 17:54:40 +0530

    Add files via upload

---
+++ p7.py
@@ -0,0 +1,20 @@
+# wapp to demo tuple
+
+t1 = (10, 30, 20, 10)
+print(t1, type(t1))
+
+t2 = ()
+print(t2, type(t2))
+
+t3 = "amit", "sumit", "neha"
+print(t3, type(t3))
+# tuples not necessary need () to express themselves
+
+# display
+
+print(t3)
+
+# to get each tuple on separate line
+
+for t in t3:
+	print(t)
